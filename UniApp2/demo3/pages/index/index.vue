2d53e161bf2de1eb1112c731c91e371b64a735f7
limwhol <57886952+limwhol@users.noreply.github.com>
2024-05-27 19:26:12 +0800

    2

---
+++ UniApp2/demo3/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="home">
+		<view class="scrollNav">
+			<scroll-view scroll-x="true" class="navscroll">
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+				<view class="item">国内</view>
+			</scroll-view>
+		</view>
+		<view class="content">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.home {
+		.scrollNav{
+			.navscroll{
+				height: 100rpx;
+				background-color: #f5f5f5;
+				white-space: nowrap;
+::-webkit-scrollbar {
+    display: none;
+}
+				.item{
+					font-size: 30rpx;
+					display: inline-block;
+					line-height: 100rpx;
+					padding: 0 30rpx;
+				}
+			}
+		}
+	}
+</style>
---
+++ UniApp2/demo3/pages/user/user.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<view class="">
+			个人中心
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
