4b127a8875a140a453439d20c6c76ad28fb228b8
limwhol <57886952+limwhol@users.noreply.github.com>
2024-07-02 23:46:58 +0800

    2

--- UniApp2/UniId1/pages/demo2/demo2.vue
+++ UniApp2/UniId1/pages/demo2/demo2.vue
@@ -1,10 +1,21 @@
 <template>
 	<view>
-		<unicloud-db v-slot:default="{data, loading, error, options}" collection="article">
+		<unicloud-db v-slot:default="{data, loading, error, options}" 
+		collection="article,uni-id-users" 
+		orderby="_id desc" 
+		field="title,content,userid.username,userid.avatar_file">
+
 			<view v-if="error">{{error.message}}</view>
+			<view v-else-if="loading">
+				<uni-load-more status="loading"></uni-load-more>
+			</view>
 			<view v-else>
 				<uni-list>
-					<uni-list-item v-for="item in data" :key="item._id" :title="item.title" :note="item.content" :rightText="item.posttime"></uni-list-item>
+					<uni-list-item v-for="item in data" :key="item._id" 
+					:title="item.title" 
+					:note="item.content" 
+					:rightText="item.userid[0].username" 
+					:thumb="item.userid[0].avatar_file.url"></uni-list-item>
 				</uni-list>
 			</view>
 		</unicloud-db>
