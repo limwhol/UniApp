b76a77205005e155f1b24d164f30ee0a36a07f78
limwhol <57886952+limwhol@users.noreply.github.com>
2024-07-03 20:01:29 +0800

    2

---
+++ UniApp2/Quanzi/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="home">
+		张华大傻逼
+		 <text class="iconfont icon-collection-fill"></text>
+		 <text class="iconfont">&#xe88e;&#xe8e3;</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.home {
+	}
+</style>
--- UniApp2/UniId1/pages/demo2/demo2.vue
+++ UniApp2/UniId1/pages/demo2/demo2.vue
@@ -1,9 +1,7 @@
 <template>
 	<view>
-		<unicloud-db v-slot:default="{data, loading, error, options}" 
-		collection="article,uni-id-users" 
-		orderby="_id desc" 
-		field="title,content,userid.username,userid.avatar_file">
+		<unicloud-db v-slot:default="{data, loading, error, options}" collection="article,uni-id-users"
+			orderby="_id desc" field="title,content,userid.username,userid.avatar_file" loadtime="auto" ref="udb">
 
 			<view v-if="error">{{error.message}}</view>
 			<view v-else-if="loading">
@@ -11,14 +9,16 @@
 			</view>
 			<view v-else>
 				<uni-list>
-					<uni-list-item v-for="item in data" :key="item._id" 
-					:title="item.title" 
-					:note="item.content" 
-					:rightText="item.userid[0].username" 
-					:thumb="item.userid[0].avatar_file.url"></uni-list-item>
+					<uni-list-item v-for="item in data" :key="item._id" :title="item.title" :note="item.content"
+						:rightText="item.userid[0].username" :thumb="item.userid[0].avatar_file.url" clickable
+						@click="onRemove(item._id)"></uni-list-item>
 				</uni-list>
 			</view>
 		</unicloud-db>
+		<view class="">
+			<!-- <button @click="getData">获取数据</button> -->
+			<button @click="addData">添加数据</button>
+		</view>
 	</view>
 </template>
 
@@ -26,11 +26,27 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+			// getData() {
+			// 	this.$refs.udb.loadData()
+			// },
+			onRemove(id) {
+				this.$refs.udb.remove(id)
+			},
+			addData() {
+				let obj = {
+					title: "小肥猪",
+					content: "小猫咪喵喵叫"
+				}
+				this.$refs.udb.add(obj)
+				this.$refs.udb.refresh()
+			}
+		},
+		onReady() {
+
 		}
 	}
 </script>
